7bf48fe01824cb052eb11e156614fef0cd313f48
erikthegamer1242 <71411196+erikthegamer1242@users.noreply.github.com>
2023-04-01 20:09:58 +0200

    Added the bare minimum to start working
    Added all the Image assets we need.
    Added some example button code, just to try it out.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,5 +14,20 @@ public partial class MainPage : ContentPage
 		
         //SemanticScreenReader.Announce(CounterBtn.Text); ispis na gumbu
 	}
+    //TODO: https://stackoverflow.com/questions/70797331/how-to-create-a-single-click-event-for-multiple-buttons-in-net-maui, POGLEDAJ TO DA NAPRAVIS 1 EVENT LISTENER ZA SVE GUMBE
+	//TODO: IMAS JOS I EMPTY.SVG KOJI MORAS POSTAVIT NA SVAKI GUMB, TO JE SAMO PRAZAN FILE, ALI NEKI SOURCE MORA BITI NA GUMBU
+	//TODO: SLIKE SU TI U Resources>Images FODLERU, I JOS IMAS ASSETS FOLDER SA ORIGINALIMA AKO NESTO SJEBES
+    void OnImageButtonClicked(object sender, EventArgs e)
+    {
+		if (count == 0) {
+            dada.Source = "krizic45.svg";
+			count = 1;
+        }
+		else {
+            dada.Source = "krizic.svg";
+			count = 0;
+        }
+		
+    }
 }
 
